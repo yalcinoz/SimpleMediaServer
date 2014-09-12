cd59ee928f948fb0fc99d45de2116d4e8d0802f7
yalcin <yalcinozveren@gmail.com>
2014-09-12 15:45:03 +0300

    simplified compile.sh

--- compile.sh
+++ compile.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-javac dash/Base.java
-javac dash/Controller.java
-javac dash/ControllerWorker.java
-javac dash/Server.java
-javac dash/ServerWorker.java
-javac dash/Main.java
-
+javac src/dash/*.java
